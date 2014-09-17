8896a50415bf029d08c0ed25addeff429c74c7b3
danhodge <dan@swipely.com>
2014-09-16 22:14:36 -0400

    enable ruby & yaml modes by default

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -27,11 +27,11 @@
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 ;; (require 'prelude-python)
-;; (require 'prelude-ruby)
+(require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
-;; (require 'prelude-yaml)
+(require 'prelude-yaml)
