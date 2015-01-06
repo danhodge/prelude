c2209ee70a638803ef9eacb2b153c26365855712
danhodge <dan@swipely.com>
2015-01-06 10:31:49 -0500

    add rspec-mode and yasnippet

--- personal/personal.el
+++ personal/personal.el
@@ -9,3 +9,16 @@
 ;; enable line numbers for all programming modes
 (add-hook 'prog-mode-hook '(lambda ()
                              (linum-mode 1)))
+
+(setq dh-packages '(rspec-mode yasnippet))
+
+(prelude-require-packages dh-packages)
+
+'(require 'rspec-mode)
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(eval-after-load 'rspec-mode
+  '(rspec-install-snippets))
+
+;;; personal.el ends here
